0685d624706c8ffd046742ba7052dd8ec1308ac1
kazi Tareq Aziz <mdazizkhn@gmail.com>
2022-10-24 01:16:44 +0600

    Add project files.

---
+++ SignalRCurdApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRCurdApp.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { 
+        
+        }
+        public virtual DbSet<Products> Products { get; set; }
+    }
+}
---
+++ SignalRCurdApp/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SignalRCurdApp.Models
+{
+    public class Products
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? Catagory { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int StockQty { get; set; }
+    }
+}
